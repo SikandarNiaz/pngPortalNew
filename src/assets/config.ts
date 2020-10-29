c9296875ac831b96ead147ec9a25074c21ee03d8
Sikandar Niaz <sikandar.niaz@marksman.com.pk>
2020-10-29 18:34:44 +0500

    config changes

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,11 +1,11 @@
 import { environment } from 'src/environments/environment';
 
 export const config = {
-      ip: 'http://pg.rtdtradetracker.com/',
-    //  ip: 'http://localhost:8080/audit/',
+  ip: 'https://png.rtdtradetracker.com/',
+  // ip: 'http://localhost:8080/audit/',
+  //  ip: 'http://pg.concavetech.com/',
   hash: environment.hash,
   main_logo: 'assets/images/logo.png',
   login_theme_color: 'green',
-  login_logo: 'assets/images/logoSmall.png'
-
+  login_logo: 'assets/images/logoSmall.png',
 };
