9697490018820654b84fa1a159d2c4ac0091de07
DESKTOP-OKF980M\sikan <sikandarniaz532@gmail.com>
2021-07-12 10:10:08 +0500

    code formatted

--- src/app/layout/dashboard/evaluation/section-seven-view/section-seven-view.component.ts
+++ src/app/layout/dashboard/evaluation/section-seven-view/section-seven-view.component.ts
@@ -1,35 +1,43 @@
-import { Component, OnInit, SimpleChanges, Input, Output, EventEmitter, ViewChild } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { config } from 'src/assets/config';
-import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
-import { EvaluationService } from '../evaluation.service';
-import { ModalDirective } from 'ngx-bootstrap';
+import {
+  Component,
+  OnInit,
+  SimpleChanges,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild,
+} from "@angular/core";
+import { environment } from "src/environments/environment";
+import { config } from "src/assets/config";
+import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { EvaluationService } from "../evaluation.service";
+import { ModalDirective } from "ngx-bootstrap";
 
 @Component({
-  selector: 'section-seven-view',
-  templateUrl: './section-seven-view.component.html',
-  styleUrls: ['./section-seven-view.component.scss']
+  selector: "section-seven-view",
+  templateUrl: "./section-seven-view.component.html",
+  styleUrls: ["./section-seven-view.component.scss"],
 })
 export class SectionSevenViewComponent implements OnInit {
-
-  @Input('data') data;
+  @Input("data") data;
   // @ViewChild('childModal') childModal: ModalDirective;
-  @ViewChild('childModal') childModal: ModalDirective;
-  @Output('showModal') showModal: any = new EventEmitter<any>();
-  @Input('isEditable') isEditable: any;
-  @Output('psku') pskuForEmit: any = new EventEmitter<any>();
+  @ViewChild("childModal") childModal: ModalDirective;
+  @Output("showModal") showModal: any = new EventEmitter<any>();
+  @Input("isEditable") isEditable: any;
+  @Output("psku") pskuForEmit: any = new EventEmitter<any>();
   selectedShop: any = {};
   selectedImage: any = {};
   // ip=environment.ip;
   configFile = config;
 
   ip: any = this.configFile.ip;
-  hover = 'hover';
+  hover = "hover";
   zoomOptions = {
-    Mode: 'hover'
+    Mode: "hover",
   };
-  zoomedImage = 'https://image.shutterstock.com/image-photo/micro-peacock-feather-hd-imagebest-260nw-1127238569.jpg';
+  zoomedImage =
+    "https://image.shutterstock.com/image-photo/micro-peacock-feather-hd-imagebest-260nw-1127238569.jpg";
   products: any;
   availability: any;
   changeColor: boolean;
@@ -50,18 +58,24 @@ export class SectionSevenViewComponent implements OnInit {
   totalDesiredFacing: any;
   secondaryData: any = [];
 
-  statusArray: any = [{ title: 'Yes', value: '1' }, { title: 'No', value: '0' }];
+  statusArray: any = [
+    { title: "Yes", value: "1" },
+    { title: "No", value: "0" },
+  ];
 
-  constructor(private router: Router, private toastr: ToastrService, private httpService: EvaluationService) { }
+  constructor(
+    private router: Router,
+    private toastr: ToastrService,
+    private httpService: EvaluationService
+  ) {}
 
   ngOnInit() {
-    const arr = this.router.url.split('/');
+    const arr = this.router.url.split("/");
     this.surveyId = +arr[arr.length - 1];
-    this.evaluatorId = localStorage.getItem('user_id');
+    this.evaluatorId = localStorage.getItem("user_id");
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-
     if (changes.data.currentValue) {
       this.data = changes.data.currentValue;
       this.selectedImage = this.data.imageList[0];
@@ -70,38 +84,33 @@ export class SectionSevenViewComponent implements OnInit {
       this.secondaryData = this.data.secondaryData || [];
       this.kbdData = this.data.kbdList || [];
       if (this.products.length > 0) {
-      this.availability = this.getAvailabilityCount(this.products);
-      this.facing = this.getFacingCount(this.products);
-      this.totalDesiredFacing = this.getTotalDesiredFacing(this.products);
+        this.availability = this.getAvailabilityCount(this.products);
+        this.facing = this.getFacingCount(this.products);
+        this.totalDesiredFacing = this.getTotalDesiredFacing(this.products);
       }
-      console.log('is editable', this.isEditable);
+      console.log("is editable", this.isEditable);
       this.MSLNAvailabilityCount = this.getMSLNAvailbilityCount(this.products);
     }
-
-
   }
 
   setSelectedImage(img) {
     this.selectedImage = img;
-
   }
-
 
   getAvailabilityCount(products) {
     const sum = [];
-    products.forEach(element => {
+    products.forEach((element) => {
       // tslint:disable-next-line: triple-equals
       if (element.available_sku >= 1) {
-      sum.push(element);
+        sum.push(element);
       }
-
     });
     return sum.length;
   }
 
   getTotalDesiredFacing(products) {
     let sum = 0;
-    products.forEach(el => {
+    products.forEach((el) => {
       sum = +el.desired_facing + sum;
     });
     return sum;
@@ -109,37 +118,35 @@ export class SectionSevenViewComponent implements OnInit {
 
   getFacingCount(products) {
     let sum = 0;
-    products.forEach(el => {
+    products.forEach((el) => {
       sum = +el.face_unit + sum;
     });
     return sum;
-}
+  }
 
   getMSLNAvailbilityCount(products) {
     const pro = [];
     const msl = [];
-    products.forEach(p => {
+    products.forEach((p) => {
       let obj = {};
-     if (p.MSL === 'Yes'  && p.available_sku === 1 ) {
-       obj = {
-         available_sku: p.available_sku,
-         MSL: p.MSL
-       };
-       pro.push(obj);
+      if (p.MSL === "Yes" && p.available_sku === 1) {
+        obj = {
+          available_sku: p.available_sku,
+          MSL: p.MSL,
+        };
+        pro.push(obj);
+      }
 
-     }
-
-     if (p.MSL === 'Yes') {
-       msl.push(p);
-     }
-
-   });
-  this.MSLCount = msl.length;
-    return  pro.length;
+      if (p.MSL === "Yes") {
+        msl.push(p);
+      }
+    });
+    this.MSLCount = msl.length;
+    return pro.length;
   }
 
   updateString(value) {
-    return value ? 'Yes' : 'No';
+    return value ? "Yes" : "No";
   }
 
   changeSku(value) {
@@ -159,193 +166,175 @@ export class SectionSevenViewComponent implements OnInit {
         evaluatorId: this.evaluatorId,
       };
 
-
-
       // return value?'YES':'NO';
 
-  this.httpService.updateData(obj).subscribe((data: any) => {
-    if (data.success) {
-      this.loading = false;
-      this.toastr.success('Data Updated Successfully');
-      // this.products=data.productList;
-      const key = data.msdId;
-      this.products.forEach(e => {
+      this.httpService.updateData(obj).subscribe((data: any) => {
+        if (data.success) {
+          this.loading = false;
+          this.toastr.success("Data Updated Successfully");
+          // this.products=data.productList;
+          const key = data.msdId;
+          this.products.forEach((e) => {
+            // for (const key of this.colorUpdateList) {
+            if (key === e.id) {
+              const i = this.products.findIndex((p) => p.id === key);
+              const obj = {
+                id: e.id,
+                available_sku:
+                  e.available_sku === 0
+                    ? (e.available_sku = 1)
+                    : (e.available_sku = 0),
+                MSL: e.MSL,
+                product_title: e.product_title,
+                face_unit: e.face_unit,
+                survey_id: e.survey_id,
+                desired_facing: e.desired_facing,
+                category_title: e.category_title,
+                color: "red",
+              };
 
-      // for (const key of this.colorUpdateList) {
-        if (key === e.id) {
-          const i = this.products.findIndex(p => p.id === key);
-          const obj = {
-            id: e.id,
-            available_sku: (e.available_sku === 0) ? e.available_sku = 1 : e.available_sku = 0,
-            MSL: e.MSL,
-            product_title: e.product_title,
-            face_unit: e.face_unit,
-            survey_id: e.survey_id,
-            desired_facing: e.desired_facing,
-            category_title: e.category_title,
-            color: 'red'
-          };
+              this.products.splice(i, 1, obj);
 
-          this.products.splice(i, 1, obj);
+              // console.log(this.products[i])
+            }
+            // localStorage.setItem('assetTypeId', JSON.stringify(this.data.assetTypeId));
 
-          // console.log(this.products[i])
+            // }
+
+            this.availability = this.getAvailabilityCount(this.products);
+            this.MSLNAvailabilityCount = this.getMSLNAvailbilityCount(
+              this.products
+            );
+          });
+
+          this.pskuForEmit.emit();
+        } else {
+          this.toastr.error(data.message, "Update Data");
         }
-        // localStorage.setItem('assetTypeId', JSON.stringify(this.data.assetTypeId));
-
-
-
-
-      // }
-
-      this.availability = this.getAvailabilityCount(this.products);
-      this.MSLNAvailabilityCount = this.getMSLNAvailbilityCount(this.products);
-
       });
-
-       this.pskuForEmit.emit();
-
-    } else {
-      this.toastr.error(data.message, 'Update Data');
-    }
-  });
-
     }
   }
-
-
 
   changeFacing(value) {
-
     this.loading = true;
     if (value.face_unit !== null) {
-    if (this.isEditable) {
-      this.changeColor = true;
-      this.updatingMSL = true;
+      if (this.isEditable) {
+        this.changeColor = true;
+        this.updatingMSL = true;
 
-      this.colorUpdateList.push(value.id);
-      const obj = {
-        msdId: value.id,
-        newValue: value.face_unit,
-        categoryTitle: this.data.sectionTitle,
-        title: value.product_title,
-        type: 2,
-        surveyId: value.survey_id,
-        evaluatorId: this.evaluatorId,
-      };
+        this.colorUpdateList.push(value.id);
+        const obj = {
+          msdId: value.id,
+          newValue: value.face_unit,
+          categoryTitle: this.data.sectionTitle,
+          title: value.product_title,
+          type: 2,
+          surveyId: value.survey_id,
+          evaluatorId: this.evaluatorId,
+        };
 
+        // return value?'YES':'NO';
 
+        this.httpService.updateData(obj).subscribe((data: any) => {
+          if (data.success) {
+            this.loading = false;
+            this.toastr.success("Data Updated Successfully");
+            // this.products=data.productList;
+            const key = data.msdId;
+            this.products.forEach((e) => {
+              // for (const key of this.colorUpdateList) {
+              if (key === e.id) {
+                const i = this.products.findIndex((p) => p.id === key);
+                const obj = {
+                  id: e.id,
+                  available_sku: e.available_sku,
+                  MSL: e.MSL,
+                  product_title: e.product_title,
+                  survey_id: e.survey_id,
+                  face_unit: e.face_unit,
+                  desired_facing: e.desired_facing,
+                  category_title: e.category_title,
+                  color: "red",
+                };
 
-      // return value?'YES':'NO';
+                this.products.splice(i, 1, obj);
 
-  this.httpService.updateData(obj).subscribe((data: any) => {
-    if (data.success) {
-      this.loading = false;
-      this.toastr.success('Data Updated Successfully');
-      // this.products=data.productList;
-      const key = data.msdId;
-      this.products.forEach(e => {
+                // console.log(this.products[i])
+              }
+              // localStorage.setItem('assetTypeId', JSON.stringify(this.data.assetTypeId));
 
-      // for (const key of this.colorUpdateList) {
-        if (key === e.id) {
-          const i = this.products.findIndex(p => p.id === key);
-          const obj = {
-            id: e.id,
-            available_sku: e.available_sku,
-            MSL: e.MSL,
-            product_title: e.product_title,
-            survey_id: e.survey_id,
-            face_unit: e.face_unit,
-            desired_facing: e.desired_facing,
-            category_title: e.category_title,
-            color: 'red'
-          };
+              // }
 
-          this.products.splice(i, 1, obj);
+              this.facing = this.getFacingCount(this.products);
+              this.totalDesiredFacing = this.getTotalDesiredFacing(
+                this.products
+              );
+            });
 
-          // console.log(this.products[i])
-        }
-        // localStorage.setItem('assetTypeId', JSON.stringify(this.data.assetTypeId));
-
-
-
-
-      // }
-
-      this.facing = this.getFacingCount(this.products);
-      this.totalDesiredFacing = this.getTotalDesiredFacing(this.products);
-      });
-
-      //  this.assetTypeForEmit.emit(this.data.assetTypeId);
-
+            //  this.assetTypeForEmit.emit(this.data.assetTypeId);
+          } else {
+            this.toastr.error(data.message, "Update Data");
+            this.loading = false;
+          }
+        });
+      }
     } else {
-      this.toastr.error(data.message, 'Update Data');
+      this.toastr.error("Facing Value is Incorrect");
       this.loading = false;
     }
-  });
-
-    }
-  } else {
-    this.toastr.error('Facing Value is Incorrect');
-    this.loading = false;
-  }
   }
 
   changeUtilization(value) {
     this.loading = true;
     if (value.utilization != null) {
-    if (this.isEditable) {
+      if (this.isEditable) {
+        const obj = {
+          msdId: value.secondarySurveyId,
+          newValue: value.utilization,
+          categoryTitle: this.data.sectionTitle,
+          title: value.categoryTitle,
+          type: 3,
+          surveyId: value.survey_id,
+          evaluatorId: this.evaluatorId,
+        };
 
-      const obj = {
-        msdId: value.secondarySurveyId,
-        newValue: value.utilization,
-        categoryTitle: this.data.sectionTitle,
-        title: value.categoryTitle,
-        type: 3,
-        surveyId: value.survey_id,
-        evaluatorId: this.evaluatorId,
-      };
+        this.httpService.updateData(obj).subscribe((data: any) => {
+          if (data.success) {
+            this.loading = false;
+            this.toastr.success("Data Updated Successfully");
+            const key = data.msdId;
+            this.products.forEach((e) => {
+              // for (const key of this.colorUpdateList) {
+              if (key === e.id) {
+                const i = this.products.findIndex((p) => p.id === key);
+                // const obj = {
+                //   id: e.id,
+                //   available_sku: e.available_sku,
+                //   MSL: e.MSL,
+                //   product_title: e.product_title,
+                //   face_unit: e.face_unit,
+                //   desired_facing: e.desired_facing,
+                //   category_title: e.category_title,
+                //   color: 'red'
+                // };
 
-  this.httpService.updateData(obj).subscribe((data: any) => {
-    if (data.success) {
-      this.loading = false;
-      this.toastr.success('Data Updated Successfully');
-      const key = data.msdId;
-      this.products.forEach(e => {
+                // this.products.splice(i, 1, obj);
 
-      // for (const key of this.colorUpdateList) {
-        if (key === e.id) {
-          const i = this.products.findIndex(p => p.id === key);
-          // const obj = {
-          //   id: e.id,
-          //   available_sku: e.available_sku,
-          //   MSL: e.MSL,
-          //   product_title: e.product_title,
-          //   face_unit: e.face_unit,
-          //   desired_facing: e.desired_facing,
-          //   category_title: e.category_title,
-          //   color: 'red'
-          // };
+                // console.log(this.products[i])
+              }
 
-          // this.products.splice(i, 1, obj);
-
-          // console.log(this.products[i])
-        }
-
-      // }
-      });
-
+              // }
+            });
+          } else {
+            this.toastr.error(data.message, "Update Data");
+          }
+        });
+      }
     } else {
-      this.toastr.error(data.message, 'Update Data');
+      this.toastr.error("Utilization Value is Incorrect");
+      this.loading = false;
     }
-  });
-
-    }
-  } else {
-    this.toastr.error('Utilization Value is Incorrect');
-    this.loading = false;
   }
-  }
-
 
   showChildModal(shop): void {
     this.selectedShop = shop;
@@ -355,25 +344,23 @@ export class SectionSevenViewComponent implements OnInit {
 
   showFacingChildModal(product) {
     if (this.isEditable) {
-    this.selectedProduct = product;
-    this.childModal.show();
+      this.selectedProduct = product;
+      this.childModal.show();
+    }
   }
-
-}
- hideChildModal() {
+  hideChildModal() {
     this.childModal.hide();
   }
-  changeKbd(newVal, value ) {
+  changeKbd(newVal, value) {
     this.loading = true;
     let selectedOption;
-    for(const option of value.optionList){
-      if(newVal==option.id){
-        selectedOption=option;
+    for (const option of value.optionList) {
+      if (newVal == option.id) {
+        selectedOption = option;
         break;
       }
     }
     if (this.isEditable) {
-
       const obj = {
         msdId: value.merchQuestId,
         title: value.question,
@@ -381,44 +368,39 @@ export class SectionSevenViewComponent implements OnInit {
         newValueId: selectedOption.id,
         newValue: selectedOption.title,
         type: 4,
-        evaluatorId: this.evaluatorId
+        evaluatorId: this.evaluatorId,
       };
 
+      this.httpService.updateData(obj).subscribe((data: any) => {
+        if (data.success) {
+          this.loading = false;
+          this.toastr.success("Data Updated Successfully");
+          const key = data.msdId;
+          this.kbdData.forEach((e) => {
+            // for (const key of this.colorUpdateList) {
+            if (key === e.merchQuestId) {
+              const i = this.kbdData.findIndex((p) => p.id === key);
+              // const obj = {
+              //   merchQuestId: e.merchQuestId,
+              //   optionId: e.optionId,
+              //   MSL: e.MSL,
+              //   product_title: e.product_title,
+              //   face_unit: e.face_unit,
+              //   desired_facing: e.desired_facing,
+              //   category_title: e.category_title,
+              // };
 
-  this.httpService.updateData(obj).subscribe((data: any) => {
-    if (data.success) {
-      this.loading = false;
-      this.toastr.success('Data Updated Successfully');
-      const key = data.msdId;
-      this.kbdData.forEach(e => {
+              // this.kbdData.splice(i, 1, obj);
 
-      // for (const key of this.colorUpdateList) {
-        if (key === e.merchQuestId) {
-          const i = this.kbdData.findIndex(p => p.id === key);
-          // const obj = {
-          //   merchQuestId: e.merchQuestId,
-          //   optionId: e.optionId,
-          //   MSL: e.MSL,
-          //   product_title: e.product_title,
-          //   face_unit: e.face_unit,
-          //   desired_facing: e.desired_facing,
-          //   category_title: e.category_title,
-          // };
+              // console.log(this.products[i])
+            }
 
-          // this.kbdData.splice(i, 1, obj);
-
-          // console.log(this.products[i])
+            // }
+          });
+        } else {
+          this.toastr.error(data.message, "Update Data");
         }
-
-      // }
       });
-
-    } else {
-      this.toastr.error(data.message, 'Update Data');
-    }
-  });
-
     }
   }
 }
-
