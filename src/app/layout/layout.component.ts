3df405bf118c5a084c2aa75660422ada097f4cbf
DESKTOP-OKF980M\sikan <sikandarniaz532@gmail.com>
2021-07-29 11:37:45 +0500

    layout changes

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,42 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { filter } from 'rxjs/operators';
+import { Component, OnInit } from "@angular/core";
+import { Router, NavigationEnd } from "@angular/router";
+import { filter } from "rxjs/operators";
 
 @Component({
-    selector: 'app-layout',
-    templateUrl: './layout.component.html',
-    styleUrls: ['./layout.component.scss']
+  selector: "app-layout",
+  templateUrl: "./layout.component.html",
+  styleUrls: ["./layout.component.scss"],
 })
 export class LayoutComponent implements OnInit {
-    hideSideBar = false; // make default value to false after completing SMS manager;
-    url: any;
+  hideSideBar = false; // make default value to false after completing SMS manager;
+  url: any;
 
-    constructor(public router: Router) { }
+  constructor(public router: Router) {}
 
-    ngOnInit() {
-        this.router.events.pipe(filter(e => e instanceof NavigationEnd)
-        ).subscribe((e: NavigationEnd) => {
-            this.url = e.url;
-            // tslint:disable-next-line:triple-equals
-            if (e.url == '/dashboard/sos-dashboard' || e.url == '/dashboard/sku-dashboard' || e.url == '/dashboard/productivity-tableau' || e.url == '/dashboard/stf-dashboard') {
-                this.hideSideBar = true;
-            }
-        });
-        let url: any = new Array();
-        url = this.router.url.split('/');
-        const t: any = url.find(d => d === 'shop_detail');
-        const r: any = url.find(d => d === 'details');
-        if (t || r) {
-            this.hideSideBar = true;
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((e) => e instanceof NavigationEnd))
+      .subscribe((e: NavigationEnd) => {
+        this.url = e.url;
+        // tslint:disable-next-line:triple-equals
+        if (
+          e.url == "/dashboard/sos-dashboard" ||
+          e.url == "/dashboard/sku-dashboard" ||
+          e.url == "/dashboard/productivity-tableau" ||
+          e.url == "/dashboard/stf-dashboard"
+        ) {
+          this.hideSideBar = true;
         }
-
+      });
+    let url: any = new Array();
+    url = this.router.url.split(/[?/]/);
+    const t: any = url.find((d) => d === "shop_detail");
+    const r: any = url.find((d) => d === "details");
+    if (t || r || localStorage.getItem("user_type") == null) {
+      this.hideSideBar = true;
     }
+  }
 
-    hideBarStatus() {
-        if (this.hideSideBar === true) {
-            this.hideSideBar = false;
-        } else {
-            this.hideSideBar = true;
-        }
+  hideBarStatus() {
+    if (this.hideSideBar === true) {
+      this.hideSideBar = false;
+    } else {
+      this.hideSideBar = true;
     }
+  }
 }
