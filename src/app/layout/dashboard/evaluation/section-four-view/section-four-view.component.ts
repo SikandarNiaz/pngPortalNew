0ce2d53406d323de5682e4874a59755adf815b73
farhan.saleem <farhanrao532@gmail.com>
2022-11-01 17:32:56 +0500

    evaluation map changed

--- src/app/layout/dashboard/evaluation/section-four-view/section-four-view.component.ts
+++ src/app/layout/dashboard/evaluation/section-four-view/section-four-view.component.ts
@@ -1,31 +1,164 @@
-import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
-import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
+import { LiteralArrayExpr } from "@angular/compiler";
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  Input,
+  SimpleChanges,
+} from "@angular/core";
+import { SafeResourceUrl, DomSanitizer } from "@angular/platform-browser";
+import { Config } from "src/assets/config";
+
 declare const google: any;
+declare var ol: any;
 @Component({
-  selector: 'section-four-view',
-  templateUrl: './section-four-view.component.html',
-  styleUrls: ['./section-four-view.component.scss']
+  selector: "section-four-view",
+  templateUrl: "./section-four-view.component.html",
+  styleUrls: ["./section-four-view.component.scss"],
 })
 export class SectionFourViewComponent implements OnInit {
-
-  @Input('data') data;
-  locations: any = [];
-  centerPoint: any = [];
+  @Input("data") data;
   lat: any;
   long: any;
   mapSrc: SafeResourceUrl;
-  url: any;
+  map: any;
+  center: any;
+  locationData: any = [];
+  mapArrayIndex = 1;
 
-  constructor(public sanitizer: DomSanitizer) { }
+  ip: any = Config.BASE_URI;
+  constructor(public sanitizer: DomSanitizer) {}
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.locationData = this.data.mslTable;
+    console.log("this.mslData: ", this.data.mslTable);
+    this.center = ol.proj.transform(
+      [
+        parseFloat(this.locationData[0].longitude),
+        parseFloat(this.locationData[0].latitude),
+      ],
+      "EPSG:4326",
+      "EPSG:3857"
+    );
+    this.initialize_map();
+  }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    this.data = changes.data.currentValue;
-    this.lat = this.data.mslTable[0].latitude;
-    this.long = this.data.mslTable[0].longitude;
+  initialize_map() {
+    this.map = new ol.Map({
+      target: "map",
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM({
+            url: "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png",
+          }),
+        }),
+      ],
+      view: new ol.View({
+        center: this.center,
+        zoom: 15,
+      }),
+    });
+    for (const element of this.locationData) {
+      this.add_map_point(
+        element.latitude,
+        element.longitude,
+        this.mapArrayIndex++
+      );
+    }
+    this.mapArrayIndex = 1;
+  }
 
-    this.url = 'https://maps.google.com/maps?q=' + this.lat + '%2C' + this.long + '&t=&z=13&ie=UTF8&iwloc=&output=embed';
-    this.mapSrc = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+  add_map_point(lat, lng, index) {
+    const radius = 100;
+    const edgeCoordinate = [this.center[0] + radius, this.center[1]];
+    const sphere = new ol.Sphere(6378137);
+    const groundRadius = sphere.haversineDistance(
+      ol.proj.transform(this.center, "EPSG:3857", "EPSG:4326"),
+      ol.proj.transform(edgeCoordinate, "EPSG:3857", "EPSG:4326")
+    );
+
+    const vectorLayer = new ol.layer.Vector({
+      source: new ol.source.Vector({
+        features: [
+          new ol.Feature({
+            geometry: new ol.geom.Point(
+              ol.proj.transform(
+                [parseFloat(lng), parseFloat(lat)],
+                "EPSG:4326",
+                "EPSG:3857"
+              )
+            ),
+            id: index,
+          }),
+          new ol.Feature(
+            new ol.geom.Circle(
+              ol.proj.transform(
+                [
+                  parseFloat(this.locationData[0].longitude),
+                  parseFloat(this.locationData[0].latitude),
+                ],
+                "EPSG:4326",
+                "EPSG:3857"
+              ),
+              groundRadius
+            )
+          ),
+        ],
+      }),
+      style: new ol.style.Style({
+        image: new ol.style.Icon({
+          anchor: [0.6, 0.6],
+          anchorXUnits: "fraction",
+          anchorYUnits: "fraction",
+          src: this.ip + "/images/" + index + ".png",
+        }),
+        stroke: new ol.style.Stroke({
+          color: "red",
+          width: 3,
+        }),
+        fill: new ol.style.Fill({
+          color: "rgba(255, 0, 0, 0.1)",
+        }),
+      }),
+    });
+    this.map.addLayer(vectorLayer);
+    // this.setPopUp(this.map, popUpInfo);
+  }
+
+  setPopUp(map) {
+    const locations = this.locationData;
+    map.on('singleclick', function (event) {
+      const container = document.getElementById('popup');
+      const content = document.getElementById('popup-content');
+      const closer = document.getElementById('popup-closer');
+      const overlay = new ol.Overlay({
+        element: container,
+        autoPan: true,
+        autoPanAnimation: {
+          duration: 250,
+        },
+      });
+      map.addOverlay(overlay);
+
+      closer.onclick = function () {
+        overlay.setPosition(undefined);
+        closer.blur();
+        return false;
+      };
+      if (map.hasFeatureAtPixel(event.pixel) === true) {
+        map.forEachFeatureAtPixel(event.pixel, function (feature) {
+          if (feature.get('id')) {
+            const coordinate = event.coordinate;
+            content.innerHTML =
+              '<b>Visit Date: </b>' +
+              locations[feature.get('id') - 1].visit_datetime;
+            overlay.setPosition(coordinate);
+          }
+        });
+      } else {
+        overlay.setPosition(undefined);
+        closer.blur();
+      }
+    });
   }
 }
